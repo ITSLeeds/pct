18e298512102edc08cbefcc2f5e640ad58d90a6d
Robin Lovelace <rob00x@gmail.com>
2019-02-28 18:11:26 +0000

    Add code from minihack : )

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' Example OD data for Leeds
+#'
+#' `od_leeds` contains the 100 most travelled work desire lines in Leeds,
+#' according to the 2011 Census.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name od_leeds
+#' @examples
+#' # see data-raw folder for generation code
+#' od_leeds
+NULL
+
+#' Zone data for Leeds
+#'
+#' Zones in Leeds
+#'
+#' @docType data
+#' @keywords datasets
+#' @name zones_leeds
+#' @examples
+#' # see data-raw folder for generation code
+#' zones_leeds
+NULL
