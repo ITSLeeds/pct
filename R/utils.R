aa9015cee3ca70a29154388354176185fe637c3b
Layik <layik.hama@gmail.com>
2019-03-01 16:20:44 +0000

    Shortcut for docs was failing

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,9 @@
-# simple object to use throughout the package
-modes = list(BASE = 1,
-             UK_GOV_TARGET = 2,
-             GENDER_EQUAL = 3,
-             GO_DUTCH = 4,
-             EBIKE = 5)
-
+#' simple object to use throughout the package
+#'
+# modes = list(BASE = 1,
+#              UK_GOV_TARGET = 2,
+#              GENDER_EQUAL = 3,
+#              GO_DUTCH = 4,
+#              EBIKE = 5)
+#
 # TODO: add models to list?
