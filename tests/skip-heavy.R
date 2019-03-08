e9b561a5efbd77ebb6892449977a5698371aa100
Layik <layik.hama@gmail.com>
2019-03-08 16:03:05 +0000

    Proper skip and more tests for desire-lines code

--- tests/skip-heavy.R
+++ tests/skip-heavy.R
@@ -2,6 +2,6 @@
 #' PCT_SKIP_HEAVY env to false
 skip_heavy = function() {
   if(!curl::has_internet() |
-     !identical(Sys.getenv("PCT_RUN_HEAVY_TESTS"), "false"))
+     !identical(Sys.getenv("PCT_RUN_HEAVY_TESTS"), "true"))
     skip("No connection or not running run once tests.")
 }
--- tests/testthat/test-desire_lines.R
+++ tests/testthat/test-desire_lines.R
@@ -4,4 +4,8 @@ context("test-desire_lines")
 test_that("pct_area_desire_lines works", {
   skip_heavy()
   expect_equal(nrow(pct_area_desire_lines(n = 10)), 10)
+  expect_error(pct_area_desire_lines(area = ""))
+  expect_error(pct_area_desire_lines(area = NULL))
+  expect_error(pct_area_desire_lines(area = NA))
+  expect_error(pct_area_desire_lines(area = LETTERS))
 })
