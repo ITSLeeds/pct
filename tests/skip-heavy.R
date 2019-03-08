d1a5a018d8714913028872677ec4f96d1e862b55
Layik Hama <layik.hama@gmail.com>
2019-03-08 19:39:54 +0000

    Codecov and description

--- tests/skip-heavy.R
+++ tests/skip-heavy.R
@@ -1,7 +1,7 @@
 #' To run heavy tests set
-#' PCT_SKIP_HEAVY env to false
+#' PCT_RUN_HEAVY_TESTS env to false
 skip_heavy = function() {
   if(!curl::has_internet() |
-     !identical(Sys.getenv("PCT_RUN_HEAVY_TESTS"), "true"))
+     identical(Sys.getenv("PCT_RUN_HEAVY_TESTS"), "false"))
     skip("No connection or not running run once tests.")
 }
