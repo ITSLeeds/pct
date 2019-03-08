469e429347d1d0db98f8f8b1221f1f606bf5695d
Layik Hama <layik.hama@gmail.com>
2019-03-08 20:04:00 +0000

    Codecov

--- tests/testthat/test-scenarios.R
+++ tests/testthat/test-scenarios.R
@@ -3,10 +3,14 @@ context("test-scenarios")
 test_that("uptake_pct_govtarget(1,1) should be 0.04768831", {
   # need to compare some data
   expect_equal(uptake_pct_govtarget(1,1), 0.04768831)
+  expect_error(uptake_pct_govtarget())
+  expect_message(uptake_pct_govtarget(1001,2))
 })
 
 test_that("uptake_pct_godutch(1,1) should be round(0.341359, digits = 5)", {
   # need to compare some data
   expect_equal(round(uptake_pct_godutch(1,1), digits = 5),
                0.34136)
+  expect_error(uptake_pct_godutch())
+  expect_message(uptake_pct_godutch(1001,2))
 })
