12f7c38dcbb7d1e7ec0a66e7a6f075c7208a2198
Layik <layik.hama@gmail.com>
2019-03-08 11:55:17 +0000

    Test cov

--- tests/testthat/test-scenarios.R
+++ tests/testthat/test-scenarios.R
@@ -5,7 +5,7 @@ test_that("uptake_pct_govtarget(1,1) should be 0.04768831", {
   expect_equal(uptake_pct_govtarget(1,1), 0.04768831)
 })
 
-test_that("uptake_pct_godutch(1,1) should be 0.341359", {
+test_that("uptake_pct_godutch(1,1) should be 0.04768831", {
   # need to compare some data
-  # expect_equal(uptake_pct_godutch(1,1), 0.341359)
+  # expect_equal(uptake_pct_godutch(1,1), 0.04768831)
 })
