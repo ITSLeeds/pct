c1902b5e8f94b15c31267632657ca6c916f0cfb0
Robin Lovelace <rob00x@gmail.com>
2019-03-08 07:20:58 +0000

    Add get_centroid_ew()

---
+++ R/download.R
@@ -0,0 +1,19 @@
+#' Download MSOA centroids for England and Wales
+#'
+#' Downloads and processes data on where people live in England and Wales.
+#' See https://data.cdrc.ac.uk/dataset/cdrc-2011-population-weighted-centroids-gb
+#' @export
+#' @examples
+#' pwc = get_centroids_ew()
+#' plot(pwc[sample(nrow(pwc), 1000), ])
+get_centroids_ew = function() {
+  u = "https://opendata.arcgis.com/datasets/b0a6d8a3dc5d4718b3fd62c548d60f81_0.csv"
+  pwc = readr::read_csv(u)
+ sf::st_as_sf(x = pwc, coords = c("X", "Y"), crs = 4326)
+}
+
+
+
+# Note: this is an attempt to get the LSOA data:
+# u = "https://data.cdrc.ac.uk/dataset/e95b3bef-11c6-4d1e-bd72-a3315e6c398d/resource/90de9c16-f064-4a11-ada1-ab1ab1b5a323/download/englandwelshscotlandpwc2011.csv"
+# pwc = readr::read_csv(u)
