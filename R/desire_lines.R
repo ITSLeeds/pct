fd41b222884c72d426454448d3274754d2101d87
Layik Hama <layik.hama@gmail.com>
2019-03-09 09:06:44 +0000

    Good practices

--- R/desire_lines.R
+++ R/desire_lines.R
@@ -13,7 +13,9 @@ pct_area_desire_lines = function(area = "sheffield", n = 100) {
   # get the census file to read the trip counts
   census_file = file.path(tempdir(), "wu03ew_v2.csv")
   if(!exists(census_file)) {
-    utils::download.file("https://s3-eu-west-1.amazonaws.com/statistics.digitalresources.jisc.ac.uk/dkan/files/FLOW/wu03ew_v2/wu03ew_v2.zip",
+    utils::download.file(paste0("https://s3-eu-west-1.amazonaws.com/",
+                                "statistics.digitalresources.jisc.ac.uk",
+                                "/dkan/files/FLOW/wu03ew_v2/wu03ew_v2.zip"),
                   file.path(tempdir(), "wu03ew_v2.zip"))
     utils::unzip(file.path(tempdir(), "wu03ew_v2.zip"), exdir = tempdir())
   }
@@ -23,7 +25,7 @@ pct_area_desire_lines = function(area = "sheffield", n = 100) {
     pct::mode_names$census_name %in% names(od_all)]
   # get UK zones with msoa11cd, msoa11nm and the geom for stplanr::od2line
   zones_all = get_centroids_ew() # TODO: some warning?
-  zones = zones_all[grepl(area, zones_all$msoa11nm, ignore.case = T), ]
+  zones = zones_all[grepl(area, zones_all$msoa11nm, ignore.case = TRUE), ]
 
   od_area = od_all[od_all$geo_code1 %in% zones$msoa11cd &
                      od_all$geo_code2 %in% zones$msoa11cd, ]
