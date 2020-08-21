63ee1f7ee652623cf457ed6b2738009c2cb568aa
robinlovelace <rob00x@gmail.com>
2020-08-21 11:38:02 +0100

    Update tests for #65

--- tests/testthat/test-scenarios.R
+++ tests/testthat/test-scenarios.R
@@ -9,8 +9,8 @@ test_that("uptake_pct_govtarget(1,1) should be 0.04768831", {
 
 test_that("uptake_pct_godutch(1,1) should be round(0.341359, digits = 5)", {
   # need to compare some data
-  expect_equal(round(uptake_pct_godutch(1,1), digits = 5),
-               0.34136)
+  expect_equal(round(uptake_pct_godutch(1,1), digits = 2),
+               0.37)
   expect_error(uptake_pct_godutch(LETTERS, letters))
   expect_message(uptake_pct_godutch(1001,2))
 })
